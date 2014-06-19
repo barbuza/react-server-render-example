e73b41a23b93f07709862c9ed0416fbe6612c891
Victor Kotseruba <barbuzaster@gmail.com>
2014-06-19 17:55:13 +0400

    better logging in dev mode

--- logging.js
+++ logging.js
@@ -14,18 +14,5 @@ if (process.env.NODE_ENV == "production") {
     }
   };
 } else {
-  module.exports = {
-    info: function() {
-      console.info.apply(console, arguments);
-    },
-    debug: function() {
-      console.debug.apply(console, arguments);
-    },
-    warn: function() {
-      console.warn.apply(console, arguments);
-    },
-    error: function() {
-      console.error.apply(console, arguments);
-    }
-  };
+  module.exports = console;
 }
