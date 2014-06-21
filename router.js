ff8c78ebfdd2a3fb927f763f40dda8f3d4ee792a
Victor Kotseruba <barbuzaster@gmail.com>
2014-06-21 18:24:53 +0400

    type checks for router

--- router.js
+++ router.js
@@ -1,18 +1,41 @@
 var XRegExp = require("xregexp").XRegExp;
+var ReactComponent = require("react/lib/ReactComponent");
 var dispatch = require("./dispatch");
+var invariant = require("react/lib/invariant");
+var _ = require("lodash");
 
 var Route = function(pattern, handler) {
+  invariant(
+    _.isString(pattern),
+    "Route first arg should be string, %s given",
+    typeof pattern
+  );
+  invariant(
+    _.isFunction(handler),
+    "Route second arg should be function, %s given",
+    typeof handler
+  );
   this.source = pattern;
   this.pattern = XRegExp("^" + pattern + "$");
   this.handler = handler;
 };
 
 Route.prototype.match = function(path) {
+  invariant(
+    _.isString(path),
+    "Route#match first arg should be string, %s given",
+    typeof path
+  );
   var result = XRegExp.exec(path, this.pattern);
   return result;
 };
 
 Route.prototype.reverse = function(params) {
+  invariant(
+    _.isObject(params),
+    "Route#reverse arg should be object, %s given",
+    typeof params
+  );
   var path = this.source;
   for (var key in params) {
     if (params.hasOwnProperty(key)) {
@@ -55,6 +78,13 @@ Router.prototype.navigate = function(path) {
 };
 
 Router.prototype.attach = function(app) {
+
+  invariant(
+    ReactComponent.isValidComponent(app),
+    "Router#attach arg should be React component, %s given",
+    typeof popup
+  );
+
   this.app = app;
   var self = this;
 
@@ -76,6 +106,11 @@ Router.prototype.attach = function(app) {
 };
 
 Router.prototype.showPopup = function(popup) {
+  invariant(
+    ReactComponent.isValidComponent(popup),
+    "Router#showPopup arg should be React component, %s given",
+    typeof popup
+  );
   this.app.setProps({
     activePopup: popup
   });
@@ -88,16 +123,31 @@ Router.prototype.hidePopup = function() {
 };
 
 Router.prototype.addRoute = function(name, pattern, handler) {
+  invariant(
+    _.isString(name),
+    "Router#addRoute first arg should be string, %s given",
+    typeof name
+  );
   var route = new Route(pattern, handler);
   this.routes.unshift(route);
   this.namedRoutes[name] = route;
 };
 
 Router.prototype.set404Route = function(handler) {
+  invariant(
+    _.isFunction(handler),
+    "Router#set404Route arg should be function, %s given",
+    typeof handler
+  );
   this.handler404 = handler;
 };
 
 Router.prototype.setErrorHandler = function(handler) {
+  invariant(
+    _.isFunction(handler),
+    "Router#setErrorHandler arg should be function, %s given",
+    typeof handler
+  );
   this.errorHandler = handler;
 };
 
@@ -113,6 +163,16 @@ Router.prototype.getProps = function(path) {
 };
 
 Router.prototype.reverse = function(name, params) {
+  invariant(
+    _.isString(name),
+    "Router#reverse first arg should be string, %s given",
+    typeof name
+  );
+  invariant(
+    _.isObject(params),
+    "Router#reverse second arg should be object, %s given",
+    typeof params
+  );
   return this.namedRoutes[name].reverse(params);
 };
 
